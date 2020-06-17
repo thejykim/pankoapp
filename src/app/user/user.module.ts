4f667ee7e29be379708afb5e175f58b030519f3d
Joseph Kim <thejykim1@gmail.com>
2020-06-17 22:48:16 +0900

    Added email/password capability

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,13 +6,16 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { FacebookSigninDirective } from './facebook-signin.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective, FacebookSigninDirective],
+  declarations: [LoginPageComponent, GoogleSigninDirective, FacebookSigninDirective, EmailLoginComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
