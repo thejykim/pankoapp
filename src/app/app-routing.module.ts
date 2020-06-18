ed519ab9d7dace88cc41d29920ef7310aeee4be7
Joseph Kim <thejykim1@gmail.com>
2020-06-18 18:43:30 +0900

    Added basic database functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AuthGuard } from './user/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
+  { path: '', component: HomepageComponent},
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
+  { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule), canActivate: [AuthGuard]}
 ];
 
 @NgModule({
