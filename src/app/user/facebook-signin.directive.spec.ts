915c9124e45ffb5473b16a4e4c19c2c6e3ccede2
Joseph Kim <thejykim1@gmail.com>
2020-08-14 13:48:14 -0400

    Removed scroll bars from planner

--- src/app/user/facebook-signin.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FacebookSigninDirective } from './facebook-signin.directive';
-
-describe('FacebookSigninDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FacebookSigninDirective();
-    expect(directive).toBeTruthy();
-  });
-});
